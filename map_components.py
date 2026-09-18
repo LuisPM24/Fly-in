66ec3137bd1e5a0bc660414f3bb1125a8dc1f590
Luis Palomino Morón <lpalominom4c@gmail.com>
2026-09-18 16:55:54 +0200

    Fixes at map_components and map_parser

--- map_components.py
+++ map_components.py
@@ -4,21 +4,80 @@ from typing import Any
 class Hub:
     def __init__(self, is_start: bool = False, is_end: bool = False,
                  name: str = "", x: int = 0, y: int = 0,
-                 properties: dict[str, Any] = {}) -> None:
-        self.is_start = is_start
-        self.is_end = is_end
+                 properties: dict[str, Any] | None = None,) -> None:
+        self.is_start: bool = is_start
+        self.is_end: bool = is_end
         self.name = name
         self.x = x
         self.y = y
-        self.properties = properties
+        self.properties: dict[str, Any] = (properties if
+                                           properties is not None else {})
 
         self.validate_hub()
 
     def validate_hub(self) -> None:
         if self.is_start and self.is_end:
-            raise ValueError("A hub can't be 'start_hub' and 'end_hub'")
-        if not self.name.isspace():
-            raise ValueError("Invalid name")
-        for prop in list(self.properties.keys()):
-            if prop.isspace():
-                raise ValueError(f"Invalid metadata at '{prop}' hub")
+            raise ValueError("A hub can't be 'start_hub' and 'end_hub' "
+                             "at the same time")
+
+        if not self.name or "-" in self.name or " " in self.name:
+            raise ValueError(f"Invalid name: '{self.name}'")
+
+        valid_properties: set[str] = {
+            "zone",
+            "color",
+            "max_drones",
+        }
+
+        for prop, value in self.properties.items():
+            if prop not in valid_properties:
+                raise ValueError(f"Invalid metadata '{prop}' at "
+                                 f"'{self.name}' hub")
+
+            if prop == "zone":
+                self.validate_zone(value)
+            elif prop == "color":
+                self.validate_color(value)
+            elif prop == "max_drones":
+                self.validate_max_drones(value)
+
+    def validate_zone(self, value: Any) -> None:
+        valid_zones: set[str] = {
+            "normal",
+            "blocked",
+            "restricted",
+            "priority"
+        }
+
+        if not isinstance(value, str) or value not in valid_zones:
+            raise ValueError(f"Invalid zone '{value}' at '{self.name}' hub")
+
+    def validate_color(self, value: Any) -> None:
+        valid_colors: set[str] = {
+            "green",
+            "blue",
+            "yellow",
+            "red",
+            "pink",
+            "purple"
+        }
+
+        if (not isinstance(value, str) or
+           not value or any(char.isspace() for char in value) or
+           value not in valid_colors):
+            raise ValueError(f"Invalid color '{value}' at '{self.name}' hub")
+
+        if not value or any(char.isspace() for char in value):
+            raise ValueError(f"Invalid color '{value}' at '{self.name}' hub")
+
+    def validate_max_drones(self, value: Any) -> None:
+        if self.is_start or self.is_end:
+            return
+
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise ValueError(f"Invalid max_drones '{value}' at "
+                             f"'{self.name}' hub")
+
+        if value <= 0:
+            raise ValueError(f"Invalid max_drones '{value}' at "
+                             f"'{self.name}' hub")
--- map_parser.py
+++ map_parser.py
@@ -9,23 +9,76 @@ class MapParser:
 
         self.map: str = arguments.selected_map
         self.visual: str = arguments.visual_representation
-        self.starthub: Hub | None = None
-        self.endhub: Hub | None = None
-        self.hubs: list[Hub] = []
+        self.start_hub: Hub | None = None
+        self.end_hub: Hub | None = None
+        self.hubs: dict[str, Hub] = {}
         # self.connections: list[connection] = []
         # self.validate_hubs()
         self.get_hubs()
+        print(list(self.hubs))
 
     def get_hubs(self) -> None:
         with open(self.map, 'r') as file:
             for line in file:
-                if line.startswith("#"):
+                if (line.startswith("#") or line.startswith("nb_drones:")
+                   or line.startswith("connection:") or line.isspace()):
                     continue
-                if line.startswith("start_hub:"):
-                    self.get_starthub(line)
+                elif line.startswith("start_hub:"):
+                    if self.start_hub is not None:
+                        raise ValueError("Two or more start_hub definitions")
 
-    def get_starthub(self, line: str) -> None:
+                    new_hub: Hub = self.get_hub("start_hub:", line)
+                    self.add_hub(new_hub)
+                    self.start_hub = new_hub
+
+                elif line.startswith("end_hub:"):
+                    if self.end_hub is not None:
+                        raise ValueError("Two or more end_hub definitions")
+
+                    new_hub = self.get_hub("end_hub:", line)
+                    self.add_hub(new_hub)
+                    self.end_hub = new_hub
+
+                elif line.startswith("hub:"):
+                    new_hub = self.get_hub("hub:", line)
+                    self.add_hub(new_hub)
+                else:
+                    raise ValueError(f"Invalid line at map definition: {line}")
+
+            if self.start_hub is None:
+                raise ValueError("No start_hub at map definition")
+            if self.end_hub is None:
+                raise ValueError("No end_hub at map definition")
+
+    def add_hub(self, hub: Hub) -> None:
+        if hub.name in self.hubs:
+            raise ValueError(
+                f"Two or more hubs with the same name: '{hub.name}'")
+        self.hubs[hub.name] = hub
+
+    def get_hub(self, to_search: str, line: str) -> Hub:
         if not line:
-            return
-        line = line.replace("start_hub:", "", 1)
-        print(line)
+            raise ValueError(f"Invalid line: '{line}'")
+
+        new_line = line.replace(to_search, "", 1)
+        words = new_line.split()
+
+        if len(words) < 3:
+            raise ValueError(f"Invalid line: '{line}'")
+
+        is_start: bool = False
+        is_end: bool = False
+
+        if to_search == "start_hub:":
+            is_start = True
+        elif to_search == "end_hub:":
+            is_end = True
+
+        try:
+            name: str = words[0]
+            x_value: int = int(words[1])
+            y_value: int = int(words[2])
+
+            return Hub(is_start, is_end, name, x_value, y_value)
+        except ValueError as e:
+            raise ValueError(e)
